7f3a889d484adbc4ee9f1987a48462c20fe52c02
irakeshbind <rakeshbind351998@gmail.com>
2026-01-04 16:37:35 +0530

    create form element

--- frontend/sbs/src/App.jsx
+++ frontend/sbs/src/App.jsx
@@ -3,8 +3,10 @@ import Signup from "./components/Signup";
 import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import './components/style.css'
 const App = () => {
   const myRouter = createBrowserRouter([
+    { path: "/", Component: Login },
     { path: "login", Component: Login },
     {path:"signup",Component:Signup},
     {path:"dashboard",Component:Dashboard},
--- frontend/sbs/src/components/Signup.jsx
+++ frontend/sbs/src/components/Signup.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
 
 const Signup = () => {
   return (
-    <div>Signup</div>
-  )
-}
+    <div className="signup-wrapper">
+      <div className="signup-box">
+        <div className="signup-left-box">
+          <img
+            className="signup-image"
+            src="/assets/img1.png"
+            alt="Institute Management"
+          />
 
-export default Signup
+          <h1 className="signup-left-heading">Institute Management</h1>
+          <p className="paragraph-text">
+            Manage Your All data in easy way...
+          </p>
+        </div>
+
+        <div className="signup-right-box">
+          
+          <form className="signup-fom">
+            <h1>Create your account</h1>
+            <input className=" type="text" placeholder="Institute name"/>
+             <input type="text" placeholder="Email"/>
+              <input type="text" placeholder="Phone"/>
+               <input type="text" placeholder="Password"/>
+               <button>submit</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
