98e8cf3bf60943e3958c09130b0c9b0d9696d51c
irakeshbind <rakeshbind351998@gmail.com>
2026-01-04 14:46:51 +0530

    routes create

--- frontend/sbs/src/App.jsx
+++ frontend/sbs/src/App.jsx
@@ -1,17 +1,19 @@
-import React, { Component } from 'react'
-import Signup from './components/Signup'
-
+import React, { Component } from "react";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 const App = () => {
   const myRouter = createBrowserRouter([
-    {path:"login",Component:Login},
-    {path:"signup",Component:Signup},
-    {path:"dashboard",Component:Dashboard},
-  ])
+    { path: "/login", Component: Login },
+    {path:"/signup",Component:Signup},
+    {path:"/dashboard",Component:Dashboard},
+  ]);
   return (
     <div>
-      <RoouterProvide route={myRouter}/>
+      <RouterProvider router={myRouter} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
