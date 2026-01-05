550630a69fe53c9a224ecd808e2796c4d80f1b94
irakeshbind <rakeshbind351998@gmail.com>
2026-01-05 12:59:17 +0530

    databse connection

---
+++ backend/app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const authRoute = require("./Routes/auth");
+
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
+const cors = require("cors");
+const connectWithDatabase = async () => {
+  try {
+    // console.log(process.env.TOKEN_KEY)
+    await mongoose.connect(
+      "mongodb+srv://rakesh:123@cluster0.lydcv.mongodb.net/"
+    );
+    console.log("connected with databases");
+  } catch (err) {
+    console.log("databse is not connect");
+  }
+};
+connectWithDatabase();
+app.use(cors());
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
+app.use(bodyParser.json());
+
+app.use("/user", authRoute);
+
+module.exports = app;
