b528f0ad474ce4a218ada33978f556a63a910bb6
irakeshbind <rakeshbind351998@gmail.com>
2026-01-05 11:05:25 +0530

    state create in the form element

--- frontend/sbs/src/components/Signup.jsx
+++ frontend/sbs/src/components/Signup.jsx
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Signup = () => {
+  // state create
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [Phone, setPhone] = useState("");
+  const [Password, setPassword] = useState("");
+  //  file
+  const [image, setImage] = useState(null);
+
+  // submit
+  const submitHandler=(event)=>{
+    event.preventDefault()
+    console.log(fullName,email,Phone,Password,image)
+
+
+  }
+  // file
+  const fileHandler=(e)=>{
+    setImage(e.target.files[0])
+  }
   return (
     <div className="signup-wrapper">
       <div className="signup-box">
@@ -12,20 +31,18 @@ const Signup = () => {
           />
 
           <h1 className="signup-left-heading">Institute Management</h1>
-          <p className="paragraph-text">
-            Manage Your All data in easy way...
-          </p>
+          <p className="paragraph-text">Manage Your All data in easy way...</p>
         </div>
 
         <div className="signup-right-box">
-          
-          <form className="signup-fom">
+          <form className="signup-fom" onSubmit={submitHandler}>
             <h1>Create your account</h1>
-            <input type="text" placeholder="Institute name"/>
-             <input type="text" placeholder="Email"/>
-              <input type="text" placeholder="Phone"/>
-               <input type="text" placeholder="Password"/>
-               <button className="button-text">submit</button>
+            <input onChange={e=>{setFullName(e.target.value)}} type="text" placeholder="Institute full name" />
+            <input onChange={e=>{setEmail(e.target.value)}} type="email" placeholder="Email" />
+            <input onChange={e=>{setPhone(e.target.value)}} type="text" placeholder="Phone" />
+            <input onChange={e=>{setPassword(e.target.value)}} type="password" placeholder="Password" />
+            <input onChange={fileHandler} type="file" />
+            <button className="button-text" type="submit">submit</button>
           </form>
         </div>
       </div>
